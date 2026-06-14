a8360459be8f2eea6bfd90149c053b42ebccaf1b
Ali Hamza <alihamzaahmad680@gmail.com>
2026-06-14 19:02:00 +0500

    fix: complete code cleanup and safe image upload

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL =
-  import.meta.env.VITE_BACKEND_URL || "https://server-kohl-nine-68.vercel.app/";
+  // import.meta.env.VITE_BACKEND_URL || "https://server-kohl-nine-68.vercel.app/";
+  import.meta.env.VITE_BACKEND_URL || "https://greencart-iota-one.vercel.app";
 
 export const AppContext = createContext();
 
